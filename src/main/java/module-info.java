3d6a26ef5af0b0cc2663124de29125483dea32d9
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:59:57 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-task-style - Default style for tasks nested within SemanticCMS pages and elements.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,13 +21,13 @@
  * along with pragmatickm-task-style.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.pragmatickm.task.style {
-	exports com.pragmatickm.task.style;
-	// Direct
-	requires com.aoapps.web.resources.registry; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-registry</artifactId>
-	requires com.aoapps.web.resources.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-servlet</artifactId>
-	requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
-	requires com.pragmatickm.task.model; // <groupId>com.pragmatickm</groupId><artifactId>pragmatickm-task-model</artifactId>
-	requires com.semanticcms.core.renderer.html; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer-html</artifactId>
-	// Transitive
-	requires com.semanticcms.core.renderer; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer</artifactId>
+  exports com.pragmatickm.task.style;
+  // Direct
+  requires com.aoapps.web.resources.registry; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-registry</artifactId>
+  requires com.aoapps.web.resources.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-web-resources-servlet</artifactId>
+  requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
+  requires com.pragmatickm.task.model; // <groupId>com.pragmatickm</groupId><artifactId>pragmatickm-task-model</artifactId>
+  requires com.semanticcms.core.renderer.html; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer-html</artifactId>
+  // Transitive
+  requires com.semanticcms.core.renderer; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-renderer</artifactId>
 }
