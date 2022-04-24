55a8b61ecf3e63f01d76dc01b278b64c1f9bd907
AO Industries, Inc <support@aoindustries.com>
2022-04-24 14:43:30 -0500

    Performed automatic source code clean-up with rewrite-maven-plugin

--- src/main/java/com/pragmatickm/task/style/TaskStyle.java
+++ src/main/java/com/pragmatickm/task/style/TaskStyle.java
@@ -47,10 +47,10 @@ public class TaskStyle implements ServletContextListener {
 
     // Add our CSS file
     RegistryEE.Application.get(servletContext)
-      .activate(RESOURCE_GROUP) // TODO: Activate as-needed
-      .getGroup(RESOURCE_GROUP)
-      .styles
-      .add(PRAGMATICKM_TASK);
+        .activate(RESOURCE_GROUP) // TODO: Activate as-needed
+        .getGroup(RESOURCE_GROUP)
+        .styles
+        .add(PRAGMATICKM_TASK);
 
     HtmlRenderer htmlRenderer = HtmlRenderer.getInstance(servletContext);
     // Add link CSS class
